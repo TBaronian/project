af57586cc0a11dda1283730274978b71bab051da
TBaronian <65422476+TBaronian@users.noreply.github.com>
2022-02-25 20:32:42 +0000

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import csv
-
-data_matrix = np.load("data_1.npz", allow_pickle=True)
-with open('test.csv', 'w') as writefile:
-    writer = csv.writer(writefile)
-    writer.writerows(data_matrix['arr_0'][1])
-
-writefile.close()
